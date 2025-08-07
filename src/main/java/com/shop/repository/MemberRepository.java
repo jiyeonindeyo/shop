34ef0268d3640e380b3f2e765789b2168fa10386
jiyeonindeyo <zbvldi123@gmail.com>
2025-08-07 17:41:40 +0900

    회원가입테스트

---
+++ src/main/java/com/shop/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.shop.repository;
+
+import com.shop.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Member findByEmail(String email); //email이 unique하기 때문에 리스트로 받지않아도 됨
+
+
+}
